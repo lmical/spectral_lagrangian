ee246627d009f4b4589cec277cdc5d64403e0f8f
lmical <lorenzomicalizzi@gmail.com>
2023-06-27 11:57:51 -0600

    Oder of the basis functions modified to be 0,1,2,...

--- code/reference_element.py
+++ code/reference_element.py
@@ -161,7 +161,7 @@ def basis_functions(type_basis,degree):
             phi1=np.vectorize(phi1)
             phi=np.append(phi,phi1)
 
-        elif degree==2: #0---2---1#
+        elif degree==2: #0---1---2#
 
             #x1=xi     barycentric coordinate of the right node
             #x2=1-xi   barycentric coordinate of the left node
@@ -176,19 +176,19 @@ def basis_functions(type_basis,degree):
             def phi1(xi):
             	x1=xi
             	x2=1-xi
-            	return x1*(x1-x2)
+            	return 4*x1*x2
             phi1=np.vectorize(phi1)
             phi=np.append(phi,phi1)
 
             def phi2(xi):
             	x1=xi
             	x2=1-xi
-            	return 4*x1*x2
+            	return x1*(x1-x2)
             phi2=np.vectorize(phi2)
             phi=np.append(phi,phi2)
 
 
-        elif degree==3: #0--2--3--1#
+        elif degree==3: #0--1--2--3#
 
             #x1=xi     barycentric coordinate of the right node
             #x2=1-xi   barycentric coordinate of the left node
@@ -206,14 +206,14 @@ def basis_functions(type_basis,degree):
             def phi1(xi):
             	x1=xi
             	x2=1-xi
-            	return x1*(x1-alpha)*(x1-beta)/alpha/beta
+            	return x1*(x1-beta)*(x1-1)/alpha/(-2*alpha+1)/beta
             phi1=np.vectorize(phi1)
             phi=np.append(phi,phi1)
 
             def phi2(xi):
             	x1=xi
             	x2=1-xi
-            	return x1*(x1-beta)*(x1-1)/alpha/(-2*alpha+1)/beta
+            	return x1*(x1-alpha)*(x1-1)/alpha/(2*alpha-1)/beta 
             phi2=np.vectorize(phi2)
             phi=np.append(phi,phi2)
 
@@ -221,12 +221,12 @@ def basis_functions(type_basis,degree):
             def phi3(xi):
             	x1=xi
             	x2=1-xi
-            	return x1*(x1-alpha)*(x1-1)/alpha/(2*alpha-1)/beta
+            	return x1*(x1-alpha)*(x1-beta)/alpha/beta
             phi3=np.vectorize(phi3)
             phi=np.append(phi,phi3)
 
 
-        elif degree==4: #0--2-3-4--1#
+        elif degree==4: #0--1-2-3--4#
 
             #x1=xi     barycentric coordinate of the right node
             #x2=1-xi   barycentric coordinate of the left node
@@ -244,14 +244,14 @@ def basis_functions(type_basis,degree):
             def phi1(xi):
             	x1=xi
             	x2=1-xi
-            	return (2*x1*(-alpha + x1)*(x1 - 0.5)*(x1 - beta))/(alpha*beta)
+            	return (x1*(x1-1)*(x1-0.5)*(x1-beta))/(-alpha*(2*alpha-1)*beta*(alpha-0.5))
             phi1=np.vectorize(phi1)
             phi=np.append(phi,phi1)
 
             def phi2(xi):
             	x1=xi
             	x2=1-xi
-            	return (x1*(x1-1)*(x1-0.5)*(x1-beta))/(-alpha*(2*alpha-1)*beta*(alpha-0.5))
+            	return -(4*x1*(alpha -x1)*(x1-1)*(x1-beta))/(alpha-0.5)**2
             phi2=np.vectorize(phi2)
             phi=np.append(phi,phi2)
 
@@ -259,14 +259,14 @@ def basis_functions(type_basis,degree):
             def phi3(xi):
             	x1=xi
             	x2=1-xi
-            	return  -(4*x1*(alpha -x1)*(x1-1)*(x1-beta))/(alpha-0.5)**2
+            	return  -(x1*(alpha-x1)*(x1-1)*(x1-0.5))/(-alpha*(2*alpha-1)*beta*(alpha-0.5))
             phi3=np.vectorize(phi3)
             phi=np.append(phi,phi3)
 
             def phi4(xi):
             	x1=xi
             	x2=1-xi
-            	return -(x1*(alpha-x1)*(x1-1)*(x1-0.5))/(-alpha*(2*alpha-1)*beta*(alpha-0.5))
+            	return (2*x1*(-alpha + x1)*(x1 - 0.5)*(x1 - beta))/(alpha*beta)
             phi4=np.vectorize(phi4)
             phi=np.append(phi,phi4)
         else:
